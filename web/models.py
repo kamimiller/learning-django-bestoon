960ce9734872ae5f15a006872d965919ebd61763
kamimiller <mrkamimiller@outlook.com>
2023-02-10 14:45:27 +0330

    incomme in models and some beatification

--- web/models.py
+++ web/models.py
@@ -7,3 +7,15 @@ class Expense(models.Model):
     date = models.DateField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.date}-{self.amount}"
+    
+class Income(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.date}-{self.amount}"
